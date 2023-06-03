65269866cf9c6a1db87d891b35eca7300f72c000
Jrson <45284565+jrson83@users.noreply.github.com>
2023-06-03 16:31:40 +0200

    feat(web): remove npx debug logs

--- packages/web/lib/index.cjs
+++ packages/web/lib/index.cjs
@@ -1,11 +1,6 @@
 const { join } = require('node:path')
 
 function resolveWebRoot() {
-  console.log('process.cwd():', process.cwd())
-  console.log('__dirname:', __dirname)
-  console.log('__filename:', __filename)
-  console.log('process.mainModule:', require.main)
-  console.log(join(__dirname, '..', 'dist'))
   return join(__dirname, '..', 'dist')
 }
 
