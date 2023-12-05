3ae07d9e574bb41cffd0d4c140fb77ee13bc5382
Jrson <45284565+jrson83@users.noreply.github.com>
2023-12-05 23:06:26 +0100

    fix(savescum): remove unused error function

--- packages/savescum/src/utils/handleError.ts
+++
@@ -1,8 +0,0 @@
-import { error } from './messages'
-
-export function handleError(err: unknown) {
-  if (err instanceof Error) {
-    error(`${err.name}: ${err.message}`)
-    process.exit(1)
-  }
-}
